d02198ba2256acdcdbc8caef8db878ffc32a19e9
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-24 18:58:09 -0400

    Completed

---
+++ 00-greetFriend/greetFriend.js
@@ -0,0 +1,8 @@
+function greetFriend(name) {
+  // Insert code here;
+  //document.write("Hello" + name);
+  return "Hello, " + name + "!";
+}
+
+// Do not edit this line;
+module.exports = greetFriend;
