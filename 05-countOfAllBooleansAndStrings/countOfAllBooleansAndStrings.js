0272cb4e173d91edeee3017c37d0efca6340bcb6
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-25 11:01:39 -0400

    Completed

---
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -0,0 +1,13 @@
+function countOfAllBooleansAndStrings(arr) {
+  // Insert code here;
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === true || arr[i] === false || typeof arr[i] === "string") {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Do not edit this line;
+module.exports = countOfAllBooleansAndStrings;
