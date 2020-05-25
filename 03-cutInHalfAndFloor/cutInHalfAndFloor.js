eca60f1a843877c888dc1d666c7aa41f9a74b0c5
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-24 23:48:01 -0400

    Completed

---
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -0,0 +1,8 @@
+function cutInHalfAndFloor(num) {
+  // Insert code here;
+  //var half = num / 2;
+  return Math.floor(num / 2);
+}
+
+// Do not edit this line;
+module.exports = cutInHalfAndFloor;
