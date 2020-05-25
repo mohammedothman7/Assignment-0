90a769134fb4159d18442f174e8d7a6bdfe25080
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-24 23:34:49 -0400

    Completed

---
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -0,0 +1,8 @@
+function titleCaseEdit(title) {
+  // Insert code here;
+  return title.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+// Do not edit this line;
+module.exports = titleCaseEdit;
