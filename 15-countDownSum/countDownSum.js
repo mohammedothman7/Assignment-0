960988e32baed171039e3e79aad5a1cc385ab3ee
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-26 01:22:11 -0400

    Completed

---
+++ 15-countDownSum/countDownSum.js
@@ -0,0 +1,14 @@
+class MySolution {
+  countDownSum(num) {
+    // Insert code here;
+    if (num <= 1) {
+      return num;
+    }
+
+    return this.countDownSum(num - 1) + num;
+  }
+}
+
+// Do not edit this line;
+let StudentSolution = MySolution;
+module.exports = StudentSolution;
