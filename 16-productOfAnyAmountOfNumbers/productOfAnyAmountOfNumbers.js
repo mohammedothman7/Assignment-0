2342524441949a75a1c78ae50c82aea4cce59cdf
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-05-26 01:31:17 -0400

    Completed

---
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -0,0 +1,9 @@
+function productOfAnyAmountOfNumbers(...args) {
+  // Insert code here;
+  return args.reduce((previous, current) => {
+    return previous * current;
+  });
+}
+
+// Do not edit this line;
+module.exports = productOfAnyAmountOfNumbers;
